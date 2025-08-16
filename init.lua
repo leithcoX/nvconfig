ac9406bb7106b460ea720d75192eeefef57fa322
aromero3003 <alromero@fi.uba.ar>
2025-08-16 13:19:46 -0300

    feat: Abro el archivo en la línea donde se cerró el archivo

--- init.lua
+++ init.lua
@@ -1,3 +1,12 @@
 require("options")
 require("lazy_setup")
 require("keymaps")
+
+vim.api.nvim_create_autocmd({"BufReadPost"}, {
+    pattern = {"*"},
+    callback = function()
+        if vim.fn.line("'\"") > 1 and vim.fn.line("'\"") <= vim.fn.line("$") then
+            vim.api.nvim_exec("normal! g'\"",false)
+        end
+    end
+})
