ec6f9ad07f4f79bf736682ca06fed71f808d7714
aromero3003 <alromero@fi.uba.ar>
2025-08-17 20:19:02 -0300

    refactor(plugins): Aumento la prioridad del colorscheme

--- lua/options.lua
+++ lua/options.lua
@@ -7,6 +7,7 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
 vim.opt.number = true
+vim.opt.relativenumber = true
 vim.opt.wrap = false
 
 vim.opt.undofile = true
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,5 +1,6 @@
 return {
     "rebelot/kanagawa.nvim",
+    priority = 60,
     config = function()
         vim.cmd "colorscheme kanagawa-wave"
     end,
