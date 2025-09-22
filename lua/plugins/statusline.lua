7c9a99d78f65092ea478595dbccda5bc976642f0
aromero3003 <alromero@fi.uba.ar>
2025-09-22 02:19:12 -0300

    feat: Agrego winbar con la jerarquía semántica del scope activo

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -1,5 +1,6 @@
 return {
 	"rebelot/heirline.nvim",
+	dependencies = { { "SmiteshP/nvim-navic", dependencies = { "neovim/nvim-lspconfig" } } },
 	-- lazy = false,
 	-- priority = 49,
 	config = function()
@@ -134,9 +135,9 @@ return {
 				local extension = vim.fn.fnamemodify(filename, ":e")
 				self.icon, self.icon_color =
 					require("nvim-web-devicons").get_icon_color(filename, extension, { default = true })
-				if not self.is_active then
-					self.icon_color = "gray"
-				end
+				-- if not self.is_active then
+				-- 	self.icon_color = "gray"
+				-- end
 			end,
 			provider = function(self)
 				return self.icon and (self.icon .. " ")
@@ -202,7 +203,7 @@ return {
 			FileIcon,
 			utils.insert(FileNameModifer, FileName), -- a new table where FileName is a child of FileNameModifier
 			FileFlags,
-			{ provider = "%<" } -- this means that the statusline is cut here when there's not enough space
+			{ provider = "%<" }             -- this means that the statusline is cut here when there's not enough space
 		)
 
 		local FileType = {
@@ -531,8 +532,135 @@ return {
 			TablineBufferBlock,
 			{ provider = "", hl = { fg = "gray" } }, -- left truncation, optional (defaults to "<")
 			{ provider = "", hl = { fg = "gray" } } -- right trunctation, also optional (defaults to ...... yep, ">")
-			-- by the way, open a lot of buffers and try clicking them ;)
+		-- by the way, open a lot of buffers and try clicking them ;)
 		)
+
+		local WinBars = {
+			fallthrough = false,
+			{ -- A special winbar for terminals
+				condition = function()
+					return conditions.buffer_matches({ buftype = { "terminal" } })
+				end,
+				utils.surround({ "", "" }, "dark_red", {
+					FileType,
+					Space,
+					TerminalName,
+				}),
+			},
+			{ -- An inactive winbar for regular files
+				condition = function()
+					return not conditions.is_active()
+				end,
+				utils.surround({ "", "" }, "bright_bg", { hl = { fg = "gray", force = true }, FileNameBlock }),
+			},
+			-- A winbar for regular files
+			utils.surround({ "", "" }, "bright_bg", FileNameBlock),
+		}
+
+		local WinBar = {
+			init = function(self)
+				self.bufnr = vim.api.nvim_get_current_buf()
+			end,
+			fallthrough = false,
+			-- conditions =
+		}
+
+		local Navic = {
+			condition = function()
+				return require("nvim-navic").is_available()
+			end,
+			static = {
+				-- create a type highlight map
+				type_hl = {
+					File = "Directory",
+					Module = "@include",
+					Namespace = "@namespace",
+					Package = "@include",
+					Class = "@structure",
+					Method = "@method",
+					Property = "@property",
+					Field = "@field",
+					Constructor = "@constructor",
+					Enum = "@field",
+					Interface = "@type",
+					Function = "@function",
+					Variable = "@variable",
+					Constant = "@constant",
+					String = "@string",
+					Number = "@number",
+					Boolean = "@boolean",
+					Array = "@field",
+					Object = "@type",
+					Key = "@keyword",
+					Null = "@comment",
+					EnumMember = "@field",
+					Struct = "@structure",
+					Event = "@keyword",
+					Operator = "@operator",
+					TypeParameter = "@type",
+				},
+				-- bit operation dark magic, see below...
+				enc = function(line, col, winnr)
+					return bit.bor(bit.lshift(line, 16), bit.lshift(col, 6), winnr)
+				end,
+				-- line: 16 bit (65535); col: 10 bit (1023); winnr: 6 bit (63)
+				dec = function(c)
+					local line = bit.rshift(c, 16)
+					local col = bit.band(bit.rshift(c, 6), 1023)
+					local winnr = bit.band(c, 63)
+					return line, col, winnr
+				end,
+			},
+			init = function(self)
+				local data = require("nvim-navic").get_data() or {}
+				local children = {}
+				-- create a child for each level
+				for i, d in ipairs(data) do
+					-- encode line and column numbers into a single integer
+					local pos = self.enc(d.scope.start.line, d.scope.start.character, self.winnr)
+					local child = {
+						{
+							provider = d.icon,
+							hl = self.type_hl[d.type],
+						},
+						{
+							-- escape `%`s (elixir) and buggy default separators
+							provider = d.name:gsub("%%", "%%%%"):gsub("%s*->%s*", ""),
+							-- highlight icon only or location name as well
+							-- hl = self.type_hl[d.type],
+
+							on_click = {
+								-- pass the encoded position through minwid
+								minwid = pos,
+								callback = function(_, minwid)
+									-- decode
+									local line, col, winnr = self.dec(minwid)
+									vim.api.nvim_win_set_cursor(vim.fn.win_getid(winnr), { line, col })
+								end,
+								name = "heirline_navic",
+							},
+						},
+					}
+					-- add a separator only if needed
+					if #data > 1 and i < #data then
+						table.insert(child, {
+							provider = " > ",
+							hl = { fg = "bright_fg" },
+						})
+					end
+					table.insert(children, child)
+				end
+				-- instantiate the new child, overwriting the previous one
+				self.child = self:new(children, 1)
+			end,
+			-- evaluate the children containing navic components
+			provider = function(self)
+				return self.child:eval()
+			end,
+			hl = { fg = "gray" },
+			update = "CursorMoved",
+		}
+
 		require("heirline").setup({
 			opts = {
 				colors = colors,
@@ -547,10 +675,10 @@ return {
 			},
 			tabline = { BufferLine },
 			statusline = {
-				{ ViMode, GitBranch, FileNameBlock, GitDiff, Diagnostics, Align },
-				{ LSPActive, Sep, Ruler, Space, ScrollBar, Space },
+				{ ViMode,    GitBranch, FileNameBlock, GitDiff, Diagnostics, Align },
+				{ LSPActive, Sep,       Ruler,         Space,   ScrollBar,   Space },
 			},
-			{},
+			winbar = Navic,
 		})
 	end,
 }
